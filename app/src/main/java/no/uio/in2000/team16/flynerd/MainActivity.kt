a2f26fbd4c15e0671e5b7a4762f2852a24a375e1
mohmo <mohmo@uio.no>
2021-04-01 01:09:31 +0200

    Hi I made testing to see if it work

--- app/src/main/java/no/uio/in2000/team16/flynerd/MainActivity.kt
+++ app/src/main/java/no/uio/in2000/team16/flynerd/MainActivity.kt
@@ -1,11 +1,23 @@
 package no.uio.in2000.team16.flynerd
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    var me = 0
+    var r = 7
+    val tag = "hello"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var me = 5 + r
+
+        Log.d(tag, me.toString())
+
+
+
     }
 }
