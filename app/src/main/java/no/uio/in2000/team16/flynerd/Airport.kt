3693e2db59bdf0323dedb8ca6004fa41c1abc0b8
KaiBytes <gjchocopasta@gmail.com>
2021-04-01 19:14:02 +0200

    added airport class, csv reader and database files

---
+++ app/src/main/java/no/uio/in2000/team16/flynerd/Airport.kt
@@ -0,0 +1,18 @@
+package no.uio.in2000.team16.flynerd
+
+class Airport(val ICAO : String,
+              val name : String,
+              val country : String,
+              val latitude : Double,
+              val longtitude : Double,
+              val currentMETAR : String? = null){
+
+    override fun toString(): String {
+        return  "ICAO" + ":".padStart(10) + "$ICAO\n" +
+                "name" + ":".padStart(10) + "$name\n" +
+                "country" + ":".padStart(7) + "$country\n" +
+                "latitude" + ":".padStart(6) + "$latitude\n" +
+                "longtitude" + ":".padStart(4) + "$longtitude\n";
+    }
+
+}
--- app/src/main/java/no/uio/in2000/team16/flynerd/MainActivity.kt
+++ app/src/main/java/no/uio/in2000/team16/flynerd/MainActivity.kt
@@ -3,17 +3,40 @@ package no.uio.in2000.team16.flynerd
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import com.github.doyaaaaaken.kotlincsv.dsl.csvReader
 
 class MainActivity : AppCompatActivity() {
 
-    
+    var domesticAirportList : MutableList<Airport?> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val tsvReader = csvReader {
+            charset = "UTF-8"
+            quoteChar = '"'
+            delimiter = ';'
+            escapeChar = '\\'
+        }
 
+        tsvReader.open(resources.openRawResource(R.raw.domair)) {
+            readAllAsSequence().forEach { row ->
+                append_to_domesticAirportList(create_Airport(row[0], row[2], row[1], row[3].toDouble(), row[4].toDouble()))
+            }
+        }
 
+        for (airport in domesticAirportList){
+//            println(airport.toString())
+        }
+    }
 
+    fun append_to_domesticAirportList(airportObject : Airport?) {
+        domesticAirportList.add(airportObject)
+    }
 
+    fun create_Airport(ICAO : String, name : String, country : String, latitude : Double, longtitude : Double) : Airport? {
+        var tmpAirport : Airport? = Airport(ICAO, name, country, latitude, longtitude);
+        return tmpAirport
     }
 }
